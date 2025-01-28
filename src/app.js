f4c02e68d7157046fcbf63c12ebc1cde7eca6750
Moksh <moksh8516@gmail.com>
2025-01-28 14:32:07 +0530

    update app.js

--- src/app.js
+++ src/app.js
@@ -20,7 +20,7 @@ app.use(express.urlencoded({
 app.use(cors({
   origin: process.env.CORS_ORIGIN,
   methods: "GET,PUT,PATCH,POST,DELETE",
-  Credential: true,
+  credentials: true,
 }))
 app.use(express.static("public"))
 app.use(cookieParser())
